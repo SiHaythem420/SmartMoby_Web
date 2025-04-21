e88a6fd2da567a20c0557ffccaf62b06b5865bb5
hedigoui <goui.hedi@esprit.tn>
2025-04-21 18:01:26 +0100

    API maps

---
+++ src/Service/GoogleMapsService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+class GoogleMapsService
+{
+    private string $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+} 
