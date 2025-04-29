c528eaa1071645bea01f2001e24804b66dbf1697
Hazem Charef <charef.hazem@esprit.tn>
2025-04-29 11:00:17 +0100

    profanity

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\Avis;
 use App\Entity\Blog;
 use App\Form\AvisType;
+use App\Service\ProfanityFilterService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,29 +25,45 @@ class AvisController extends AbstractController
     }
 
     #[Route('/blog/{id}/avis/new', name: 'avis_new', methods: ['GET', 'POST'])]
-public function new(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
-{
-    $avi = new Avis();
-    $avi->setBlog($blog);
-    $avi->setDate(new \DateTime());
-    
-    $form = $this->createForm(AvisType::class, $avi);
-    $form->handleRequest($request);
+    public function new(
+        Request $request,
+        Blog $blog,
+        EntityManagerInterface $entityManager,
+        ProfanityFilterService $profanityFilter
+    ): Response {
+        $avi = new Avis();
+        $avi->setBlog($blog);
+        $avi->setDate(new \DateTime());
+        
+        $form = $this->createForm(AvisType::class, $avi);
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-        $entityManager->persist($avi);
-        $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get the comment text from the form data
+            $commentContent = $form->get('comment')->getData();
+            
+            // Alternative: If your form field is named differently, use:
+            // $commentContent = $avi->getComment(); // or whatever your method is
+            
+            // Check for profanity
+            if ($profanityFilter->containsProfanity($commentContent)) {
+                $this->addFlash('error', 'Your comment contains inappropriate language. Please modify it.');
+                return $this->redirectToRoute('avis_new', ['id' => $blog->getId()]);
+            }
+            
+            // If no profanity, save the comment
+            $entityManager->persist($avi);
+            $entityManager->flush();
 
-        // Redirect to the blog show page instead of comments index
-        return $this->redirectToRoute('blog_show', ['id' => $blog->getId()], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('blog_show', ['id' => $blog->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('avis/newComment.html.twig', [
+            'avi' => $avi,
+            'form' => $form,
+            'blog' => $blog,
+        ]);
     }
-
-    return $this->renderForm('avis/newComment.html.twig', [
-        'avi' => $avi,
-        'form' => $form,
-        'blog' => $blog,
-    ]);
-}
 
     #[Route('/avis/{id}', name: 'avis_delete', methods: ['POST'])]
     public function delete(Request $request, Avis $avi, EntityManagerInterface $entityManager): Response
